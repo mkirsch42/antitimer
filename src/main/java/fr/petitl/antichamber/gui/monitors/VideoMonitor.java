6295c03b9b3f4ae23bb821e077ad0a379b5f6087
Mathew Kirschbaum <mkirsch42@gmail.com>
2017-08-01 20:04:14 -0400

    Error when VLC not found

--- src/main/java/fr/petitl/antichamber/gui/monitors/VideoMonitor.java
+++ src/main/java/fr/petitl/antichamber/gui/monitors/VideoMonitor.java
@@ -4,6 +4,7 @@ import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import fr.petitl.antichamber.Configuration;
 import fr.petitl.antichamber.gui.MonitorFrame;
@@ -17,71 +18,84 @@ import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
  */
 public class VideoMonitor extends JFrame implements MonitorFrame {
 
-    private EmbeddedMediaPlayerComponent component;
-    private EmbeddedMediaPlayer player;
-    private boolean setup = false;
-    private long startTime = 0;
-    
-    public VideoMonitor() {
-        super("Video");
-        this.setSize(640, 360);
-        
-        new NativeDiscovery().discover();
-        
-        component = new EmbeddedMediaPlayerComponent();
-        player = component.getMediaPlayer();
-        getFrame().setContentPane(component);
-        
-        this.addComponentListener(new ComponentAdapter() {
-            @Override
-            public void componentShown(ComponentEvent e) {
-                super.componentShown(e);
-                setupPlayer();
-            }
+	private EmbeddedMediaPlayerComponent component;
+	private EmbeddedMediaPlayer player;
+	private boolean setup = false;
+	private long startTime = 0;
 
-            @Override
-            public void componentHidden(ComponentEvent e) {
-                super.componentHidden(e);
-                stopPlayer();
-            }
-        });
-    }
+	public VideoMonitor() {
+		super("Video");
+		this.setSize(640, 360);
 
-    private void setupPlayer() {
-    	Configuration conf = Configuration.read();
-        
-    	startTime = conf.getVideoStart();
-    	
-        player.stop();
-        player.prepareMedia(conf.getVideoPath());
-        player.parseMedia();
-        
-        setup = true;
-    }
+		boolean found = new NativeDiscovery().discover();
+		found = false;
+		if (!found) {
+			JFrame me = this;
+			this.addComponentListener(new ComponentAdapter() {
+				@Override
+				public void componentShown(ComponentEvent e) {
+					super.componentShown(e);
+					JOptionPane.showMessageDialog(me, "VLC not found! Video capabilities not available!");
+				}
+			});
+			return;
+		}
 
-    private void stopPlayer() {
-        player.stop();
-        setup = false;
-    }
+		component = new EmbeddedMediaPlayerComponent();
+		player = component.getMediaPlayer();
+		getFrame().setContentPane(component);
 
-    @Override
-    public JFrame getFrame() {
-        return this;
-    }
+		this.addComponentListener(new ComponentAdapter() {
+			@Override
+			public void componentShown(ComponentEvent e) {
+				super.componentShown(e);
+				setupPlayer();
+			}
 
-    @Override
-    public void update(GameStatus status) {
-    	if(status.isCurrentlyRunning()) {
-    		if(!player.isPlaying() && setup) {
-    			System.out.println("start video");
-    			player.play();
-    			player.setTime(startTime);
-    		}
-    	} else {
-    		if(setup) {
-    			System.out.println("stop video");
-    			player.stop();
-    		}
-    	}
-    }
+			@Override
+			public void componentHidden(ComponentEvent e) {
+				super.componentHidden(e);
+				stopPlayer();
+			}
+		});
+	}
+
+	private void setupPlayer() {
+		Configuration conf = Configuration.read();
+
+		startTime = conf.getVideoStart();
+
+		player.stop();
+		player.prepareMedia(conf.getVideoPath());
+		player.parseMedia();
+
+		setup = true;
+	}
+
+	private void stopPlayer() {
+		player.stop();
+		setup = false;
+	}
+
+	@Override
+	public JFrame getFrame() {
+		return this;
+	}
+
+	@Override
+	public void update(GameStatus status) {
+		if (setup) {
+			if (status.isCurrentlyRunning()) {
+				if (!player.isPlaying()) {
+					System.out.println("start video");
+					player.play();
+					player.setTime(startTime);
+				}
+			} else {
+				System.out.println("stop video");
+				player.stop();
+			}
+		}
+
+	}
 }
